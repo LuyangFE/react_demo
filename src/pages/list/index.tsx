dfd5f7a8169a2e56f73442b3f5adefe20f906548
陆洋 <luyang@luyangdeMac-mini.local>
2022-12-31 13:41:27 +0800

    format code

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getMaterialList } from '../../api';
-import { Spin } from 'antd';
+import { Spin, Result, Button, FloatButton } from 'antd';
 import Card from './components/card';
 import './style.css';
 
@@ -23,33 +23,66 @@ const List = () => {
     setData(res);
   };
 
+  async function fetchData() {
+    setSpinning(true);    
+    const result = await getMaterialList();
+    setData(result);
+    setSpinning(false);
+  }
+
   const [data, setData] = useState<Array<any>>([]);
 
   const [spinning, setSpinning] = useState<boolean>(true);
   
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await getMaterialList();
-      setData(result);
-      setSpinning(false);
-    }
     fetchData();
   }, []);
 
   return (
-    <Spin tip="Loading..." spinning={ spinning } delay={ 500 }>
+    <Spin tip="Loading..." spinning={ spinning } delay={ 100 }>
       <div style={{ minHeight: '100vh' }}>
+        <FloatButton
+          type="primary"
+          shape="square"
+          description="Form"
+          style={{ 'width': 100, 'right': 'calc(50% - 50px)' }}
+          href="/form"
+        />
         <div className="cardList">
           {
-            data.map((item: data, index) => {
-              return (
-                <Card
-                  key={ item.id }
-                  item={ item }
-                  onClick={ () => del(index) }
-                ></Card>
-              )
-            })
+            data.length > 0 ? (
+              data.map((item: data, index) => {
+                return (
+                  <Card
+                    key={ item.id }
+                    item={ item }
+                    onClick={ () => del(index) }
+                  ></Card>
+                )
+              })
+            )
+            : spinning === false ?
+            (
+              <div className="center">
+                <Result
+                  title="加载完成，数据为空"
+                  extra={
+                    <Button type="primary" onClick={ fetchData }>
+                      重新加载
+                    </Button>
+                  }
+                />
+              </div>
+            )
+            :
+            (
+              <div className="center">
+                <Result
+                  status="404"
+                  title="No Data"
+                />
+              </div>
+            )
           }
         </div>
       </div>
